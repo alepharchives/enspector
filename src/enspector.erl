cb3ea3c17d80034804aecec0e9e028065668588f
Haitao Li <lihaitao@gmail.com>
2011-07-16 18:35:50 +0800

    WebSocket works

--- src/enspector.erl
+++ src/enspector.erl
@@ -3,22 +3,22 @@
 -export([start/0, start/2, stop/1]).
 
 start() ->
-	application:start(cowboy),
-	application:start(enspector).
+    application:start(cowboy),
+    application:start(enspector).
 
 start(_Type, _Args) ->
-	Dispatch = [
+    Dispatch = [
 		{'_', [
-			{[<<"websocket">>], enspector_websocket_handler, []},
-			{[<<"static">>, '...'], enspector_static_handler, []},
-			{'_', enspector_default_handler, []}
-		]}
-	],
-	cowboy:start_listener(http, 2,
-		cowboy_tcp_transport, [{port, 8080}],
-		cowboy_http_protocol, [{dispatch, Dispatch}]
-	),
-	enspector_sup:start_link().
+                       {[<<"enspector">>], enspector_websocket_handler, []},
+                       {[<<"static">>, '...'], enspector_static_handler, []},
+                       {'_', enspector_default_handler, []}
+                      ]}
+               ],
+    cowboy:start_listener(http, 2,
+                          cowboy_tcp_transport, [{port, 8080}],
+                          cowboy_http_protocol, [{dispatch, Dispatch}]
+                         ),
+    enspector_sup:start_link().
 
 stop(_State) ->
-	ok.
+    ok.
--- src/enspector_websocket_handler.erl
+++ src/enspector_websocket_handler.erl
@@ -4,69 +4,53 @@
 -export([init/3, handle/2, terminate/2]).
 -export([websocket_init/3, websocket_handle/3, websocket_terminate/3]).
 
+-compile(export_all).
+
 init({_Any, http}, Req, []) ->
-	case cowboy_http_req:header('Upgrade', Req) of
-		{undefined, Req2} -> {ok, Req2, undefined};
-		{<<"WebSocket">>, _Req2} -> {upgrade, protocol, cowboy_http_websocket}
-	end.
+    io:format("wh init~n"),
+    case cowboy_http_req:header('Upgrade', Req) of
+        {undefined, Req2} -> {ok, Req2, undefined};
+        {<<"WebSocket">>, _Req2} -> {upgrade, protocol, cowboy_http_websocket}
+    end.
 
 handle(Req, State) ->
-	{ok, Req2} = cowboy_http_req:reply(200, [{'Content-Type', <<"text/html">>}],
-%% HTML code taken from misultin's example file.
-<<"<html>
-<head>
-<script type=\"text/javascript\">
-function addStatus(text){
-	var date = new Date();
-	document.getElementById('status').innerHTML
-		= document.getElementById('status').innerHTML
-		+ date + \": \" + text + \"<br/>\";
-}
-function ready(){
-	if (\"WebSocket\" in window) {
-		// browser supports websockets
-		var ws = new WebSocket(\"ws://localhost:8080/websocket\");
-		ws.onopen = function() {
-			// websocket is connected
-			addStatus(\"websocket connected!\");
-			// send hello data to server.
-			ws.send(\"hello server!\");
-			addStatus(\"sent message to server: 'hello server'!\");
-		};
-		ws.onmessage = function (evt) {
-			var receivedMsg = evt.data;
-			addStatus(\"server sent the following: '\" + receivedMsg + \"'\");
-		};
-		ws.onclose = function() {
-			// websocket was closed
-			addStatus(\"websocket was closed\");
-		};
-	} else {
-		// browser does not support websockets
-		addStatus(\"sorry, your browser does not support websockets.\");
-	}
-}
-</script>
-</head>
-<body onload=\"ready();\">
-Hi!
-<div id=\"status\"></div>
-</body>
-</html>">>, Req),
-	{ok, Req2, State}.
+    io:format("wh handle~n"),
+    {ok, <<"nothing">>, State}.
 
 terminate(_Req, _State) ->
-	ok.
+    ok.
 
 websocket_init(_Any, Req, []) ->
-	timer:send_interval(1000, tick),
-	Req2 = cowboy_http_req:compact(Req),
-	{ok, Req2, undefined}.
+    io:format("wh ws init~n"),
+    Req2 = cowboy_http_req:compact(Req),
+    {ok, Req2, undefined}.
 
 websocket_handle(tick, Req, State) ->
-	{reply, <<"Tick">>, Req, State, hibernate};
+    {reply, <<"Tick">>, Req, State, hibernate};
 websocket_handle({websocket, Msg}, Req, State) ->
-	{reply, << "You said: ", Msg/binary >>, Req, State, hibernate}.
+    io:format("pid: ~p: wh ws handle: ~n~p~n", [self(),Msg]),
+    Reply = dispatch(Msg),
+    {reply, Reply, Req, State, hibernate}.
+
+dispatch(Msg) ->
+    {Method, Id} = parse(Msg),
+    dispatch(Method, Id).
 
 websocket_terminate(_Reason, _Req, _State) ->
-	ok.
+    ok.
+
+parse(Msg) ->
+    {struct, List} = mochijson2:decode(Msg),
+    Method = proplists:get_value(<<"method">>, List),
+    Id = proplists:get_value(<<"id">>, List),
+    {Method, Id}.
+
+dispatch(<<"Console.enable">>, Id) ->
+    pack_response(Id, ok);
+dispatch(_, Id) ->
+    pack_response(Id, unimplemented).
+
+-spec pack_response(CallId :: integer(), Msg::term()) -> ok.
+pack_response(CallId, Msg) ->
+    Json = mochijson2:encode({struct, [{<<"id">>, CallId}, {<<"result">>, Msg}]}),
+    iolist_to_binary(Json).
